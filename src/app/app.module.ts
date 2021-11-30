dff41fcb597335aa5f95267da72cc4581aa0c262
LittleGalaxyrider <bartanics.flora@gmail.com>
2021-11-30 12:06:53 +0100

    2/39 ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import {FormsModule} from "@angular/forms";
+import { AssigmentComponent } from './assigment/assigment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AssigmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/assigment/assigment.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigment',
+  templateUrl: './assigment.component.html',
+  styleUrls: ['./assigment.component.css']
+})
+export class AssigmentComponent implements OnInit {
+  username!: string;
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
